2d4bc108312cbe27ca2604ae35bfd0f50eae1f13
Teddy Tort <etc.tortorici@gmail.com>
2022-09-29 07:56:58 -0600

    built with pip
    fixed issue with setup.py pointing to the wrong dir

--- CapacitorCalculations/setup.py
+++ CapacitorCalculations/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension
 
 sfc_module = Extension('dielepy',
                        sources=['module.cpp'],
-                       include_dirs=['include', 'D:\\Github\\DielectricPythonTools\\DielectricPythonTools'],
+                       include_dirs=['include', '..\\CapacitorCalculations'],
                        library_dirs=["\\root\\project"],
                        # define_macros=[("PY_MAJOR_VERSION", "3"), ("PY_MINOR_VERSION", "10")],
                        language="c++",
@@ -13,7 +13,7 @@ setup(
     name='dielepy',
     version='0.0',
     scripts=[],
-    packages=['src'],
+    # packages=['src'],
     description='Allows for fast calculations related to dielectric spectroscopy analysis.',
     author='Teddy Tortorici',
     author_email='etc.tortorici@gmail.com',
